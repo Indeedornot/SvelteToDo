692216fbb30a14ee03ff86f299222821a0a660e6
indeedornot <gravitybartek@gmail.com>
2022-11-11 18:52:51 +0100

    feat: Added barrel imports for components

---
+++ src/components/Todo/index.ts
@@ -0,0 +1,12 @@
+export { default as TodoDisplay } from './Display/TodoDisplay.svelte';
+export { default as TodoDisplayDnd } from './Dnd/TodoDisplayDnd.svelte';
+export { default as TodoDisplayHeader } from './Display/TodoDisplayHeader.svelte';
+export { default as TodoDisplaySearchbar } from './Display/TodoDisplaySearchbar.svelte';
+
+export { default as TodoTabDnd } from './Dnd/TodoTabDnd.svelte';
+export { default as TodoTab } from './Tab/TodoTab.svelte';
+export { default as TodoTabFooter } from './Tab/TodoTabFooter.svelte';
+export { default as TodoTabHeader } from './Tab/TodoTabHeader.svelte';
+
+export { default as TodoItem } from './Item/TodoItem.svelte';
+export { default as StatusDropdown } from './Item/StatusDropdown.svelte';
